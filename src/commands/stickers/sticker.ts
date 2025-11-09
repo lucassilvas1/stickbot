f944be72b25b1d7547f2d6d7f3d28228f3af6aeb
= <=>
2025-11-09 01:38:59 -0300

    create placeholder sticker slash command handler

---
+++ src/commands/stickers/sticker.ts
@@ -0,0 +1,29 @@
+import { SlashCommandBuilder } from "discord.js";
+import type {
+  CommandAutocomplete,
+  CommandExecutor,
+} from "../../types/commands.js";
+
+export const cooldown = 2_000;
+
+export const data = new SlashCommandBuilder()
+  .setName("sticker")
+  .setDescription("Choose a sticker by title")
+  .addStringOption((opt) =>
+    opt
+      .setName("title")
+      .setDescription("Title of the sticker")
+      .setRequired(true)
+      .setAutocomplete(true)
+  );
+
+export const execute: CommandExecutor = async (interaction) => {
+  return interaction.reply("Not implemented");
+};
+
+export const autocomplete: CommandAutocomplete = async (interaction) => {
+  return interaction.respond([
+    { name: "Not implemented", value: "not_implemented" },
+    { name: "Yet", value: "yet" },
+  ]);
+};
