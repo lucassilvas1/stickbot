e3bcad5769ded003168c2bf06368fb67ea701ecb
= <=>
2025-11-14 16:26:34 -0300

    implement edit and delete sticker commands

---
+++ src/commands/stickers/deletesticker.ts
@@ -0,0 +1,36 @@
+import { SlashCommandBuilder } from "discord.js";
+import type { CommandExecutor } from "../../types/commands.js";
+import { deleteSticker } from "../../db/index.js";
+
+export const data = new SlashCommandBuilder()
+  .setName("deletesticker")
+  .setDescription("Delete a sticker by title")
+  .addStringOption((opt) =>
+    opt
+      .setName("query")
+      .setDescription("Describe the sticker to get suggestions")
+      .setRequired(true)
+      .setAutocomplete(true)
+  );
+
+export const execute: CommandExecutor = async (interaction) => {
+  const id = interaction.options.getString("query", true);
+  try {
+    const isDeleted = await deleteSticker(id);
+    if (isDeleted) {
+      return interaction.reply({ content: "Sticker successfully deleted" });
+    } else {
+      console.error("Something went wrong deleting sticker. ID: ", id);
+      return interaction.reply({
+        content: "Something went wrong while deleting sticker...",
+      });
+    }
+  } catch (error) {
+    console.error(error);
+    return interaction.reply({
+      content: "Something went wrong while deleting sticker...",
+    });
+  }
+};
+
+export { autocomplete } from "../../utils/index.js";
---
+++ src/commands/stickers/editsticker.ts
@@ -0,0 +1,87 @@
+import { MessageFlags, SlashCommandBuilder } from "discord.js";
+import type {
+  CommandAutocomplete,
+  CommandExecutor,
+} from "../../types/commands.js";
+import { getStickerById, search, updateSticker } from "../../db/index.js";
+import type { SimplifiedSticker } from "../../types/stickers.js";
+import { Constants } from "../../utils/index.js";
+
+export const data = new SlashCommandBuilder()
+  .setName("editsticker")
+  .setDescription("Edit a sticker by title")
+  .addStringOption((opt) =>
+    opt
+      .setName("query")
+      .setDescription("Describe the sticker to get suggestions")
+      .setRequired(true)
+      .setAutocomplete(true)
+  )
+  .addStringOption((opt) =>
+    opt
+      .setName("title")
+      .setDescription("Update sticker title or leave it as it is")
+      .setMinLength(Constants.MIN_TITLE_LENGTH)
+      .setMaxLength(Constants.MAX_TITLE_LENGTH)
+      .setAutocomplete(true)
+  )
+  .addStringOption((opt) =>
+    opt
+      .setName("tags")
+      .setDescription("Update sticker tags or leave them as they are")
+      .setMinLength(Constants.MIN_TAGS_LENGTH)
+      .setMaxLength(Constants.MAX_TAGS_LENGTH)
+      .setAutocomplete(true)
+  )
+  .addStringOption((opt) =>
+    opt
+      .setName("description")
+      .setDescription(
+        "Update sticker description or leave it as it is (currently unused)"
+      )
+      .setMaxLength(Constants.MAX_DESCRIPTION_LENGTH)
+  );
+
+export const execute: CommandExecutor = async (interaction) => {
+  const id = interaction.options.getString("query", true);
+  const title = interaction.options.getString("title") ?? undefined;
+  const tags = interaction.options.getString("tags") ?? undefined;
+  const description = interaction.options.getString("description") ?? undefined;
+
+  try {
+    await updateSticker(id, { title, tags, description });
+    return interaction.reply({
+      content: "Changes were successfully saved",
+      flags: MessageFlags.Ephemeral,
+    });
+  } catch (error) {
+    console.error("Could not update sticker: ", error);
+    return interaction.reply({
+      content: "Something went wrong while saving changes...",
+      flags: MessageFlags.Ephemeral,
+    });
+  }
+};
+
+export const autocomplete: CommandAutocomplete = async (interaction) => {
+  const prop = interaction.options.getFocused(true);
+  if (prop.name === "query") {
+    return interaction.respond(await search(prop.value));
+  }
+  const id = interaction.options.getString("query", true);
+  const sticker = await getStickerById(id);
+  if (!sticker) {
+    return interaction.respond([
+      {
+        name: "STICKER NOT FOUND! PICK A VALID STICKER FROM THE query SUGGESTION FIRST",
+        value:
+          "STICKER NOT FOUND! PICK A VALID STICKER FROM THE query SUGGESTION FIRST",
+      },
+    ]);
+  }
+  if (!prop.value.length) {
+    const value = sticker[prop.name as keyof SimplifiedSticker];
+    return interaction.respond([{ name: value, value }]);
+  }
+  return interaction.respond([]);
+};
