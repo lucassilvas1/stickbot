4fd23c336372a90a74d33a01c953e05b0b42a602
= <=>
2025-11-15 02:22:47 -0300

    server static assets with express js

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { Client, Collection, GatewayIntentBits } from "discord.js";
 import { env } from "./env.js";
 import { getCommands, registerEventHandlers } from "./utils/index.js";
+import express from "express";
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
@@ -14,3 +15,28 @@ client.commands = new Collection(commands);
 await registerEventHandlers(client);
 
 client.login(env.BOT_TOKEN);
+
+const assetsServer = express();
+
+if (env.VERBOSE_LOGGING) {
+  assetsServer.use((req, _, next) => {
+    console.dir(req);
+    next();
+  });
+}
+
+assetsServer.use(
+  express.static(env.ASSETS_DIR_PATH, {
+    dotfiles: "deny",
+    fallthrough: false,
+    maxAge: "30d",
+    immutable: true,
+    index: false,
+  })
+);
+
+assetsServer.listen(env.ASSETS_SERVER_PORT, () => {
+  console.log(
+    `Assets static server listening on port ${env.ASSETS_SERVER_PORT}`
+  );
+});
