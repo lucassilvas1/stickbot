6017bd512b652a6c56a07db30fd66fa8b02cc38f
= <=>
2025-12-19 22:12:33 -0300

    stop initializing db at top level to make it easier to test the app

--- src/db/dbActions.ts
+++ src/db/dbActions.ts
@@ -11,25 +11,43 @@ import {
   _updateUserPermissions,
 } from "./crud.js";
 import { _search } from "./search.js";
+import type { Kysely } from "kysely";
+import type { Database } from "../types/db.js";
 
-export const db = await initDb();
+let hasInit = false;
+let promise: Promise<Kysely<Database>> | null;
+let db: Kysely<Database>;
 
-export const insertUserPermissions = bindDbFunction(db, _insertUserPermissions);
+async function getDb() {
+  if (!hasInit) {
+    if (!promise) promise = initDb();
+    db = await promise;
+    hasInit = true;
+    promise = null;
+  }
+  return db;
+}
 
-export const getUserPermissionsById = bindDbFunction(
-  db,
-  _getUserPermissionsById
-);
+function withDb<F extends (db: Kysely<Database>, ...args: any[]) => any>(
+  fn: F
+) {
+  return async (
+    ...args: Parameters<F> extends [any, ...infer Rest] ? Rest : never
+  ): Promise<ReturnType<F>> => {
+    const db = await getDb();
+    return fn(db, ...args);
+  };
+}
 
-export const updatedUserPermissions = bindDbFunction(
-  db,
-  _updateUserPermissions
-);
-export const deleteUserPermissions = bindDbFunction(db, _deleteUserPermissions);
-
-export const insertSticker = bindDbFunction(db, _insertSticker);
-
-export const incrementStickerUsage = bindDbFunction(db, _incrementStickerUsage);
+export const insertUserPermissions = withDb(_insertUserPermissions);
+export const getUserPermissionsById = withDb(_getUserPermissionsById);
+export const updatedUserPermissions = withDb(_updateUserPermissions);
+export const deleteUserPermissions = withDb(_deleteUserPermissions);
+export const insertSticker = withDb(_insertSticker);
+export const incrementStickerUsage = withDb(_incrementStickerUsage);
+export const updateSticker = withDb(_updateSticker);
+export const deleteSticker = withDb(_deleteSticker);
+export const search = withDb(_search);
 
 export function getStickerById(id: string): ReturnType<typeof _getStickerById>;
 export function getStickerById(
@@ -37,27 +55,11 @@ export function getStickerById(
   incrementUsage: true,
   userId: string
 ): ReturnType<typeof _getStickerById>;
-export function getStickerById(
+export async function getStickerById(
   id: string,
   incrementUsage?: boolean,
   userId?: string
 ) {
-  return _getStickerById(db, id, incrementUsage as any, userId as any);
-}
-
-export const updateSticker = bindDbFunction(db, _updateSticker);
-
-export const deleteSticker = bindDbFunction(db, _deleteSticker);
-
-export const search = bindDbFunction(db, _search);
-
-type ShiftTuple<T extends any[]> = T extends [any, ...infer Rest]
-  ? Rest
-  : never;
-
-export function bindDbFunction<F extends (db: any, ...args: any[]) => any>(
-  db: Parameters<F>[0],
-  fn: F
-): (...args: ShiftTuple<Parameters<F>>) => ReturnType<F> {
-  return (...args: any[]) => fn(db, ...args);
+  const db = await getDb();
+  return _getStickerById(db, id, incrementUsage, userId);
 }
