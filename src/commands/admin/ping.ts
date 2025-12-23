a0c606c10a1a4ea9b6ab6802a6261cbb79cdc4d0
= <=>
2025-12-22 21:55:11 -0300

    add ping command

---
+++ src/commands/admin/ping.ts
@@ -0,0 +1,22 @@
+import { MessageFlags, SlashCommandBuilder } from "discord.js";
+import type { CommandData } from "../../types/commands.js";
+
+const commandData: CommandData = {
+  isGlobal: true,
+  permissions: [],
+  data: new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription("Estimates the latency between you and the BOT"),
+  async execute(interaction) {
+    const start = performance.now();
+    await interaction.deferReply({ flags: MessageFlags.Ephemeral });
+    const totalLatency = performance.now() - start;
+    return interaction.editReply({
+      content: `Total Latency: ${Math.ceil(
+        totalLatency
+      )}ms\nWebSocket Latency: ${Math.ceil(interaction.client.ws.ping)}ms`,
+    });
+  },
+};
+
+export default commandData;
