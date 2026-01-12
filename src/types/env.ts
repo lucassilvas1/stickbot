9f0b65a9d7810c9955c555cc2008abb53785be37
= <=>
2026-01-12 19:02:25 -0300

    refactor: replace booleanParser with z.stringbool()

--- src/types/env.ts
+++ src/types/env.ts
@@ -2,10 +2,6 @@ import z = require("zod");
 import { logLevelParser } from "./misc.js";
 import { stickerSearchOrderParser } from "./stickers.js";
 
-const booleanParser = z
-  .enum(["true", "false"])
-  .transform((value) => value === "true");
-
 export const envSchema = z.object({
   BOT_TOKEN: z.string(),
   APPLICATION_ID: z.string(),
@@ -18,7 +14,7 @@ export const envSchema = z.object({
   ASSETS_SERVER_PORT: z.string(),
   LOG_LEVEL: logLevelParser.default("info"),
   LOG_DIR_PATH: z.string().optional(),
-  LOG_TO_CONSOLE: booleanParser.optional(),
+  LOG_TO_CONSOLE: z.stringbool().optional(),
   MAX_LOG_AGE_DAYS: z.coerce.number().default(7),
   MAX_LOG_SIZE_MB: z.coerce.number().default(10),
   MAX_LOG_ROTATIONS: z.coerce.number().default(4),
