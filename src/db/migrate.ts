2ad09b0d6ae6cab2888c0b8c1b71b5d19b4371b5
= <=>
2026-01-14 18:49:45 -0300

    fix: kysely failing to resolve path to migration folder on windows

--- src/db/migrate.ts
+++ src/db/migrate.ts
@@ -1,17 +1,57 @@
-import * as path from "path";
+import { join } from "path";
 import { promises as fs } from "fs";
-import { Kysely, Migrator, FileMigrationProvider } from "kysely";
+import { platform } from "node:process";
+import { Kysely, Migrator, type Migration } from "kysely";
 import { logger } from "../logging/logger.js";
+import { pathToFileURL } from "node:url";
+
+function isObject(obj: unknown) {
+  return typeof obj === "object" && obj !== null;
+}
+
+function isFunction(obj: unknown) {
+  return typeof obj === "function";
+}
+
+class FileMigrationProvider {
+  constructor(private migrationFolder: string) {}
+
+  static isMigration(obj: Record<string, any>) {
+    return isObject(obj) && isFunction(obj.up);
+  }
+  async getMigrations() {
+    const migrations: Record<string, Migration> = {};
+    const files = await fs.readdir(this.migrationFolder);
+    for (const fileName of files) {
+      if (
+        fileName.endsWith(".js") ||
+        (fileName.endsWith(".ts") && !fileName.endsWith(".d.ts")) ||
+        fileName.endsWith(".mjs") ||
+        (fileName.endsWith(".mts") && !fileName.endsWith(".d.mts"))
+      ) {
+        let migrationPath = join(this.migrationFolder, fileName);
+        if (platform === "win32") {
+          migrationPath = pathToFileURL(migrationPath).toString();
+        }
+
+        const migration = await import(migrationPath);
+        const migrationKey = fileName.substring(0, fileName.lastIndexOf("."));
+
+        if (FileMigrationProvider.isMigration(migration?.default)) {
+          migrations[migrationKey] = migration.default;
+        } else if (FileMigrationProvider.isMigration(migration)) {
+          migrations[migrationKey] = migration;
+        }
+      }
+    }
+    return migrations;
+  }
+}
 
 export async function migrateToLatest(database: Kysely<any>) {
   const migrator = new Migrator({
     db: database,
-    provider: new FileMigrationProvider({
-      fs,
-      path,
-      // This needs to be an absolute path.
-      migrationFolder: path.join(import.meta.dirname, "./migrations"),
-    }),
+    provider: new FileMigrationProvider("./migrations"),
   });
 
   const { error, results } = await migrator.migrateToLatest();
